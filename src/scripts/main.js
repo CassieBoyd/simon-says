aedad6d1a3b240cc7d44b7b2dd463ce50ba715ea
Cassie Boyd <cassandra.bacon@gmail.com>
2019-10-17 09:01:04 -0500

    Updates

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,7 +8,8 @@ for (let k = 0; k < locations.length; k++) {
     let currentLocation = locations[k]
 
     if (currentLocation > 2) {
-        const invalidLocation = true
+        let invalidLocation = true
+
         if (invalidLocation) {
             console.log("This location is invalid")
         }
@@ -21,3 +22,25 @@ for (let k = 0; k < locations.length; k++) {
     total number of locations, not the invalid ones. You
     need to fix this somehow.
 */
+
+
+// const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]]
+
+// for (let k = 0; k < locations.length; k++) {
+//     const currentLocation = locations[k]
+
+//     if (currentLocation[0] > 2) {
+//         const invalidLocation = true
+//     }
+
+//     if (invalidLocation) {
+//         console.log("This location is invalid")
+//     }
+// }
+
+// /*
+//     Since k is used as the loop counter, it will be the
+//     total number of locations, not the invalid ones. You
+//     need to fix this somehow.
+// */
+// console.log(`There were ${k} invalid locations`)
